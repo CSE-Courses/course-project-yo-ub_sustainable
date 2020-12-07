b3df07d2564e486c7b948a2fab6d763b0ff9ef98
Urjitha <urjithamuthiah@gmail.com>
2020-12-06 22:30:27 -0500

    #105 changes to deafult custom challenge

--- flaskr/challenge.py
+++ flaskr/challenge.py
@@ -1,7 +1,7 @@
 newChallenge = {
-    'name': "", 
-    'description': "challengeTasks", 
-    'duration': "challengeTasks",
-    'category': "challengeTasks",
-    'impact': "challengeTasks",
-    'suggestions': "challengeTasks"}
+    'name': "My custom challenge", 
+    'description': "This is an easy and simple challenge that can reduce my carbon footprint", 
+    'duration': "daily",
+    'category': "category 3",
+    'impact': "This is a simple task with large positive impacts.",
+    'suggestions': "- Requires simple planning to be part of routine"}
