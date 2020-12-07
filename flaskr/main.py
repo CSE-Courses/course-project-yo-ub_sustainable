d091a338b06b1918e8f4ac57cad841b5df97daca
lisakanbur <36706211+lisakanbur@users.noreply.github.com>
2020-12-07 15:04:36 -0500

    Fixed SQL syntax for query into dashboard table

--- flaskr/main.py
+++ flaskr/main.py
@@ -230,7 +230,7 @@ def create_app(test_config=None):
                 session['pro_pic'] = complete_hash
                 with connection2.cursor() as cursor3:
                     cursor3.execute("INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s, %s)", (fname, lname, username, password, email, complete_hash))
-                    cursor3.execute("INSERT INTO dashboard VALUES (%s, %s, %s, %s,)", (username, "the Earth", "being sustainable", "sign up")) 
+                    cursor3.execute("INSERT INTO dashboard VALUES (%s, %s, %s, %s)", (username, "the Earth", "being sustainable", "sign up")) 
 
                 connection2.commit()
                 msg = 'You have successfully registered!'
